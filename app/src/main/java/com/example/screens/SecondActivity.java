0bbb313f4162faca70991fc21abf2779aa44cd9c
a23alera <a23alera@student.his.se>
2024-04-18 15:47:16 +0200

    Gjorde så SecondActivity hätmaden datan med hjälp av getStringExtra från intent med hjälp av samma nyckel i MainActivity.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,7 +1,7 @@
 package com.example.screens;
 
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
@@ -9,6 +9,11 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
+        String message = getIntent().getStringExtra("EXTRA_MESSAGE");
+       TextView textView = findViewById(R.id.text_view_data);
+       textView.setText(message);
+
     }
 }
