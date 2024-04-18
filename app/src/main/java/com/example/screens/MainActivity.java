bf228feb5fe164510903d09e9119b76a53c3d74c
a23alera <a23alera@student.his.se>
2024-04-18 15:45:29 +0200

    skapde en intent för att starta SecondActivit med hjälp av putExtra. La till en sträng med nycekl EXTRA_MESSAGE. Som innehåller det meddelande SecondActivity ska hämta.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
 
 import android.os.Bundle;
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonOpenSecond = findViewById(R.id.button_open_second_activity);
+        buttonOpenSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("EXTRA_MESSAGE", "DETTA ÄR FRÅN MAIN");
+                startActivity(intent);
+            }
+        });
     }
 }
